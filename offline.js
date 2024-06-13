9959978c0ccc31894c4471dc042d0e3c7b9d4907
Taglock <39584594+Taglock@users.noreply.github.com>
2024-06-13 17:33:12 +0500

    a

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1718280983,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Новая таблица - Лист1 (2).csv",
+		"Новая таблица - Лист1 (3).csv"
+	]
+}
